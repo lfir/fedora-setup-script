f0f3dfd7000b7cc71cd1dcee1e7dede35c8d37ea
Asta86 <psljp@protonmail.com>
2022-07-20 16:59:26 -0300

    Add package chromium, remove external packages code & chrome

--- fedora36-installApps.sh
+++ fedora36-installApps.sh
@@ -24,18 +24,6 @@ rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://download1.rpmfusion.org/nonfree/fedora/\
 rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
-# Enable VSCode repository
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-
-vsc=$'[code]
-name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc'
-
-echo "$vsc" | sudo tee '/etc/yum.repos.d/vscode.repo'
-
 # Update base system
 sudo dnf --refresh upgrade
 
@@ -43,12 +31,12 @@ sudo dnf --refresh upgrade
 sudo dnf groupinstall Fonts LibreOffice
 
 # Install additional applications. Array names loosely follow submenu names in KDE's app launcher
-devel=(code docker-compose git java-latest-openjdk-devel java-latest-openjdk-javadoc meld \
+devel=(docker-compose git java-latest-openjdk-devel java-latest-openjdk-javadoc meld \
   moby-engine nodejs-yarn npm pipenv python-django-bash-completion python3-devel python3-ipython \
   python3-virtualenv ShellCheck)
 games=(knights lutris q4wine steam wine winetricks)
 hw=(hddtemp kmod-wl lm_sensors lshw radeontop stress)
-inet=(discord filezilla insync ktorrent remmina wireshark)
+inet=(chromium discord filezilla insync ktorrent remmina wireshark)
 multimedia=(asciinema bchunk ffmpeg simplescreenrecorder smplayer)
 system=(akmod-VirtualBox beesu cockpit cockpit-machines cockpit-selinux dkms exfat-utils finger \
   gnome-nettool grsync grub-customizer htop iftop iotop kcron kernel-devel ksysguard ksystemlog \
@@ -61,17 +49,6 @@ sudo dnf install "${devel[@]}" "${games[@]}" "${hw[@]}" "${inet[@]}" \
 
 # Enable Flathub repository
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# Create applications' directory and switch to it
-mkdir "$HOME/Applications" && cd "$_" || return
-
-# Install Chrome rpm
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-
-sudo dnf install ./*.rpm
-
-# Remove downloaded rpms
-rm -f ./*.rpm
 
 # Add current user to administration groups
 for group in docker libvirt vboxsf vboxusers ; do
