a8d3b4ae2c8b56efcdc7232322836ca8e64918c8
Asta86 <psljp@protonmail.com>
2023-04-26 16:58:20 -0300

    Rename fedora36-installApps.sh to fedora38-installApps.sh

---
+++ fedora38-installApps.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Uninstall unused applications present in the KDE Spin. Add or remove items as needed
+sudo dnf remove akregator dnfdragora dragon elisa-player kaddressbook kde-connect kmag \
+  kmahjongg kmail kmines kmouth korganizer kpat krdc krfb kwrite plasma-systemmonitor
+
+# Enable RPMFusion repositories
+sudo sh -c "dnf install https://download1.rpmfusion.org/free/fedora/\
+rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://download1.rpmfusion.org/nonfree/fedora/\
+rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
+# Update base system
+sudo dnf --refresh upgrade
+
+# Install package groups
+sudo dnf groupinstall Fonts LibreOffice
+
+# Install additional applications. Array names loosely follow submenu names in KDE's app launcher
+devel=(docker-compose git java-latest-openjdk-devel java-latest-openjdk-javadoc meld \
+  moby-engine nodejs-yarn npm pipenv python3-devel python3-ipython python3-virtualenv ShellCheck)
+games=(lutris q4wine steam wine winetricks)
+hw=(hddtemp kmod-wl lm_sensors lshw nvtop stress)
+inet=(chromium filezilla ktorrent remmina wireshark)
+multimedia=(asciinema bchunk simplescreenrecorder smplayer)
+system=(akmod-VirtualBox beesu cockpit cockpit-machines cockpit-selinux dkms exfat-utils finger \
+  gnome-nettool grsync grub-customizer htop iftop iotop kcron kernel-devel ksysguard ksystemlog \
+  policycoreutils-gui qemu-kvm sysstat virt-manager VirtualBox VirtualBox-server)
+utils=(clamav clamav-update detox filelight gtkhash hunspell-es kate keepassxc knotes \
+  krename kruler moreutils-parallel ncdu nfoview papirus-icon-theme p7zip ranger tldr unrar vim \
+  xchm xdotool)
+
+sudo dnf install "${devel[@]}" "${games[@]}" "${hw[@]}" "${inet[@]}" \
+  "${multimedia[@]}" "${system[@]}" "${utils[@]}"
+
+# Add current user to administration groups
+for group in docker libvirt vboxsf vboxusers ; do
+  getent group "$group" || sudo groupadd "$group" ; sudo groupmems -a "$USER" -g "$group"
+done
+
+# Enable Cockpit autostart
+sudo systemctl enable --now cockpit.socket
+
+# Cleanup
+sudo dnf autoremove
+sudo dnf clean packages
+
